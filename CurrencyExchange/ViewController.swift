e43a37cc13c0776472bccebeec18009df65ba10e
Andy Chou <andychou902@gmail.com>
2019-04-09 19:51:47 +0900

    Initial Commit

---
+++ CurrencyExchange/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyExchange
+//
+//  Created by Andy Chou on 4/9/19.
+//  Copyright © 2019 Andy Chou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
